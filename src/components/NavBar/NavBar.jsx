b0be3df0952535e27daf688f2f8cc7f3d30cc69b
RomainM81 <dcromain112@gmail.com>
2022-06-29 18:32:50 +0200

    ✨ Add Navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+
+import './NavBar.css'
+
+const NavBar = () => {
+    return ( 
+        <div className="nav-bar">
+            <ul>
+                <NavLink to='/' className={'navlink'}><li>Acceuil</li></NavLink>
+                <NavLink to='/' className={'navlink'}><li>Liste des Membres</li></NavLink>
+                <NavLink to='/' className={'navlink'}><li>Sponsors</li></NavLink>
+                <NavLink to='/' className={'navlink'}><li>Coté Presse</li></NavLink>
+                <NavLink to='/' className={'navlink'}><li>La journée des Associations</li></NavLink>
+                <NavLink to='/' className={'navlink'}><li>Le tournois</li></NavLink>
+                <NavLink to='/' className={'navlink'}><li>Galleries</li></NavLink>
+                <NavLink to='/' className={'navlink'}><li>Nous Contacter</li></NavLink>
+            </ul>
+        </div>
+     );
+}
+ 
+export default NavBar;
