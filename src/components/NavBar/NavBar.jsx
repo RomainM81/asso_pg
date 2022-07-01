7a15d64aa533d3efe12214477e3703f8228cb32b
RomainM81 <dcromain112@gmail.com>
2022-07-01 12:35:35 +0200

    💄 add logo in navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,14 +6,14 @@ const NavBar = () => {
     return ( 
         <div className="nav-bar">
             <ul>
-                <NavLink to='/' className={'navlink'}><li>Acceuil</li></NavLink>
                 <NavLink to='/' className={'navlink'}><li>Liste des Membres</li></NavLink>
                 <NavLink to='/' className={'navlink'}><li>Sponsors</li></NavLink>
                 <NavLink to='/' className={'navlink'}><li>Coté Presse</li></NavLink>
+                <NavLink to='/' ><img className={'nav-logo'} src={require('./../../assets/logo_pg.jpg')} alt=""></img></NavLink>
                 <NavLink to='/' className={'navlink'}><li>La journée des Associations</li></NavLink>
                 <NavLink to='/' className={'navlink'}><li>Le tournois</li></NavLink>
                 <NavLink to='/' className={'navlink'}><li>Galleries</li></NavLink>
-                <NavLink to='/' className={'navlink'}><li>Nous Contacter</li></NavLink>
+                {/* <NavLink to='/' className={'navlink'}><li>Nous Contacter</li></NavLink> */}
             </ul>
         </div>
      );
