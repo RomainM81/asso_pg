74bf1d0956ee21f9be61af64b044cb3ee1107ccc
RomainM81 <dcromain112@gmail.com>
2022-07-12 15:30:49 +0100

    💄 change logo in navbar(test)

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,9 +7,9 @@ const NavBar = () => {
         <div className="nav-bar">
             <ul>
                 <NavLink to='/members' className={'navlink'}><li>Liste des Membres</li></NavLink>
-                <NavLink to='/' className={'navlink'}><li>Sponsors</li></NavLink>
+                <NavLink to='/sponsors' className={'navlink'}><li>Sponsors</li></NavLink>
                 <NavLink to='/' className={'navlink'}><li>Coté Presse</li></NavLink>
-                <NavLink to='/' ><img className={'nav-logo'} src={require('./../../assets/logo_pg.jpg')} alt=""></img></NavLink>
+                <NavLink to='/' ><img className={'nav-logo'} src={require('./../../assets/logo_pg2.jpg')} alt=""></img></NavLink>
                 <NavLink to='/' className={'navlink'}><li>La journée des Associations</li></NavLink>
                 <NavLink to='/' className={'navlink'}><li>Le tournois</li></NavLink>
                 <NavLink to='/' className={'navlink'}><li>Galleries</li></NavLink>
